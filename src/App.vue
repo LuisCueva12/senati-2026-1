46d4702e6fcfb7bd4167c6da1ec6121059d1624f
LuisCueva12 <cuevasuarezluisyampier@gmail.com>
2026-04-10 00:43:39 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue';
+import Hero from './components/Hero.vue';
+import Nosotros from './components/Nosotros.vue';
+</script>
+
+<script>
+
+</script>
+
+
+<template>
+  <div>
+    <Header />
+    <Hero />
+    <Nosotros />
+    <Footer />
+  </div>
+</template>
+
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+{import('taildwincss').Config}
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
