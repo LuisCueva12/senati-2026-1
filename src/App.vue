dd12135bc662265ab4469ce56cb9085df67c36f4
LuisCueva12 <cuevasuarezluisyampier@gmail.com>
2026-04-10 00:47:21 -0500

    Mi segundo commit

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Header from './components/Header.vue'
 import Footer from './components/Footer.vue';
 import Hero from './components/Hero.vue';
 import Nosotros from './components/Nosotros.vue';
+import Galeria from './components/Galeria.vue';
 </script>
 
 <script>
@@ -14,6 +15,7 @@ import Nosotros from './components/Nosotros.vue';
   <div>
     <Header />
     <Hero />
+    <Galeria />
     <Nosotros />
     <Footer />
   </div>
